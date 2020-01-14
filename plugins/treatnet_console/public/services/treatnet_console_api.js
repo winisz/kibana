9789f55b13ce4608f2dad732665e44fdb295ee7d
mrybak <maciej.rybak@comp.comp.pl>
2020-01-14 16:24:37 +0100

    API do TNC

--- plugins/treatnet_console/public/services/treatnet_console_api.js
+++ plugins/treatnet_console/public/services/treatnet_console_api.js
@@ -5,9 +5,51 @@ import { npStart } from 'ui/new_platform';
 
 const { core } = npStart;
 
-export const TNC_API_URL = core.injectedMetadata.getInjectedVar('treatnetConsoleApiUrl');
+export const TNC_API_URL = core.injectedMetadata.getInjectedVar('tncApiUrl');
 
-export const TreatnetConsoleAPI = axios.create({
-  /* Tymczasowo, na razie endpointy pytają własnego serwera, docelowo serwera TNC */
-  baseURL: chrome.addBasePath('/api/treatnet_console/')
+export const KibanaPluginAPI = axios.create({
+  baseURL: chrome.addBasePath('/')
 });
+
+const httpClient = axios.create({
+  baseURL: TNC_API_URL
+});
+
+class BaseTreatnetConsoleAPI {
+  constructor (httpClient) {
+    this._http = httpClient;
+    // TODO: tymczasowe rozwiazanie
+    this._username = core.injectedMetadata.getInjectedVar('tncApiUsername');
+    this._password = core.injectedMetadata.getInjectedVar('tncApiPassword');
+  }
+
+  _prepareHeaders () {
+    const basicAuth = 'Basic ' + btoa(this._username + ':' + this._password);
+    return { Authorization: basicAuth };
+  }
+
+  _get (url, params = {}) {
+    return this._http.get(url, {
+      params: params,
+      headers: this._prepareHeaders()
+    });
+  }
+
+  _post (url, data = {}) {
+    return this._http.post(url, data, {
+      headers: this._prepareHeaders()
+    });
+  }
+}
+
+class StixAPI extends BaseTreatnetConsoleAPI {
+  get patterns () {
+    return {
+      list: () => this._get('stix/patterns/')
+    };
+  }
+}
+
+export const TreatnetConsoleAPI = {
+  stix: new StixAPI(httpClient)
+};
