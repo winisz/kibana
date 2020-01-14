f04042b94b27ff756a1ac01fd174932e62076b2b
mrybak <maciej.rybak@comp.comp.pl>
2020-01-14 16:23:47 +0100

    Widok 404

--- plugins/treatnet_console/server/routes/routes.js
+++ plugins/treatnet_console/server/routes/routes.js
@@ -1,10 +1,36 @@
+const path = require('path');
+const HapiReactViews = require('hapi-react-views');
 
 export default function (server) {
+  /* TODO: probably there won't be any route to Hapi server, since only React app and NSC API is needed */
   server.route({
-    path: '/api/treatnet_console/example',
+    path: '/api/example',
     method: 'GET',
-    handler () {
+    handler (request, h) {
       return { time: (new Date()).toISOString() };
     }
+  });
+
+  /* register serving ReactJS templates */
+  server.views({
+    engines: {
+      js: HapiReactViews
+    },
+    path: path.join(__dirname, '..', 'views'),
+  });
+
+  /* Custom 404 template for plugin */
+  server.ext('onPreResponse', (request, h) => {
+    const response = request.response;
+    const basePath = request.getBasePath();
+
+    if (response.isBoom && response.output.statusCode === 404) { // catch 404 raised by Kibana base routes
+      return h.view('404', {
+        homeUrl: `${basePath}/app/treatnet_console`,
+        uiPublicUrl: `${basePath}/ui`,
+        nodeModulesUrl: `${basePath}/node_modules`,
+      }).code(404);
+    }
+    return h.continue;
   });
 }
--- plugins/treatnet_console/server/views/404.js
+++ plugins/treatnet_console/server/views/404.js
@@ -6,6 +6,7 @@ class NotFoundView extends React.PureComponent {
   render () {
     const uiPublicUrl = this.props.uiPublicUrl;
     const nodeModulesUrl = this.props.nodeModulesUrl;
+    const homeUrl = this.props.homeUrl;
 
     return (
       <html lang="en">
@@ -18,11 +19,33 @@ class NotFoundView extends React.PureComponent {
           <link rel="shortcut icon" href={`${uiPublicUrl}/favicons/favicon.ico`}/>
 
           <link rel="stylesheet" href={`${nodeModulesUrl}/@elastic/eui/dist/eui_theme_light.css`}/>
-          <meta charset="UTF-8"/>
+          <meta charSet="UTF-8"/>
           <title>Treatnet Console</title>
         </head>
-        <body>
-          <h1>Opps! This page cannot be found.</h1>
+        <body style={{ height: '68%' }}>
+
+          <div className="euiPage" style={{ height: 'inherit' }}>
+            <main className="euiPageBody">
+              <div
+                className="euiPanel euiPanel--paddingLarge euiPageContent euiPageContent--verticalCenter euiPageContent--horizontalCenter">
+                <div className="euiPageContentHeader euiPageContentHeader--responsive">
+                  <div className="euiPageContentHeaderSection">
+                    <h1 className="euiTitle euiTitle--large">404</h1>
+                  </div>
+                </div>
+                <div className="euiPageContentBody euiTextAlign euiTextAlign--left">
+                  <h2 className="euiTitle euiTitle--medium">Page not found</h2>
+                  <div className="euiSpacer euiSpacer--l"/>
+                  <form action={homeUrl}>
+                    <button className="euiButton euiButton--primary" id={'superButton'} type="submit">
+                      <span className="euiButton__content"><span className="euiButton__text">Home page</span></span>
+                    </button>
+                  </form>
+                </div>
+              </div>
+            </main>
+          </div>
+
         </body>
       </html>
     );
